54f67a576971922a1776d881c42b1c3e249c192a
AliRamTem <nitro110294@gmail.com>
2021-05-19 15:11:34 +0300

    добавлены логи ко всем методам

--- second/app/src/main/java/com/example/secondapp/MainActivity.java
+++ second/app/src/main/java/com/example/secondapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("SYSTEM INFO: ","МЕТОД onCreate() запущен");
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -33,33 +35,40 @@ public class MainActivity extends AppCompatActivity {
 
         public UserHolder(LayoutInflater inflater, ViewGroup viewGroup) {
             super(inflater.inflate(R.layout.activity_item, viewGroup, false));
+            Log.d("SYSTEM INFO: ","Конструктор UserHolder");
             itemTextView = itemView.findViewById(R.id.itemTextView);
         }
         public void bind(String userName) {
             itemTextView.setText(userName);
+            Log.d("SYSTEM INFO: ","МЕТОД bind() UserHolder");
         }
     }
 
     private class UserAdapter extends RecyclerView.Adapter<UserHolder> {
         List<String> userList;
         public UserAdapter(List<String> userList) {
+            Log.d("SYSTEM INFO: ","Конструктор UserAdapter");
             this.userList = userList;
         }
 
         @Override
         public UserHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            Log.d("SYSTEM INFO: ","МЕТОД onCreateViewHolder() UserHolder");
             LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
             return new UserHolder(inflater, viewGroup);
         }
 
         @Override
         public void onBindViewHolder(UserHolder userHolder, int position) {
+            Log.d("SYSTEM INFO: ","МЕТОД onBindViewHolder() UserHolder");
             String userName = userList.get(position);
             userHolder.bind(userName);
+
         }
 
         @Override
         public int getItemCount() {
+            Log.d("SYSTEM INFO: ","МЕТОД getItemCount() UserHolder");
             return userList.size();
         }
     }
