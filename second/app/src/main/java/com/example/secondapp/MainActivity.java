db8ed6b89ab3d55012746c975afc80130f89f43e
Aliev Rasul <arasul110294@gmail.com>
2021-05-22 15:32:48 +0300

    Создание БД и добавление в нее пользователей

--- second/app/src/main/java/com/example/secondapp/MainActivity.java
+++ second/app/src/main/java/com/example/secondapp/MainActivity.java
@@ -1,75 +1,116 @@
 package com.example.secondapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private UserAdapter userAdapter;
-
+    RecyclerView recyclerView;
+    UserAdapter userAdapter;
+    Button addUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("SYSTEM INFO: ","МЕТОД onCreate() запущен");
         setContentView(R.layout.activity_main);
+        addUser = findViewById(R.id.addUser);
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        Users users = new Users();
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddUserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        recyclerViewInit();
+        Log.d("SYSTEM INFO: ", "Метод onResume() запущен");
+    }
+
+    private void recyclerViewInit(){
+        Users users = Users.get(MainActivity.this);
         List<String> userList = users.getUserList();
         userAdapter = new UserAdapter(userList);
         recyclerView.setAdapter(userAdapter);
-
     }
 
-    private class UserHolder extends RecyclerView.ViewHolder {
-        private TextView itemTextView;
 
+    // UserHolder отвечает за каждый элемент списко по отдельности
+    // Именно здесь мы будем наполнчть нашу activity_item контентом
+    private class UserHolder extends RecyclerView.ViewHolder{
+        TextView itemTextView;
         public UserHolder(LayoutInflater inflater, ViewGroup viewGroup) {
-            super(inflater.inflate(R.layout.activity_item, viewGroup, false));
-            Log.d("SYSTEM INFO: ","Конструктор UserHolder");
+            super(inflater.inflate(R.layout.activity_item,viewGroup,false));
+            //itemView - текущий layout activity_item
             itemTextView = itemView.findViewById(R.id.itemTextView);
         }
-        public void bind(String userName) {
+        public void bind(String userName){
             itemTextView.setText(userName);
-            Log.d("SYSTEM INFO: ","МЕТОД bind() UserHolder");
         }
     }
-
-    private class UserAdapter extends RecyclerView.Adapter<UserHolder> {
+    // UserAdapter нужен для того, чтобы разместить элементы на RecyclerView
+    private class UserAdapter extends RecyclerView.Adapter<UserHolder>{
         List<String> userList;
         public UserAdapter(List<String> userList) {
-            Log.d("SYSTEM INFO: ","Конструктор UserAdapter");
             this.userList = userList;
         }
 
         @Override
         public UserHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            Log.d("SYSTEM INFO: ","МЕТОД onCreateViewHolder() UserHolder");
+            // inflater - наполнитель
             LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-            return new UserHolder(inflater, viewGroup);
+            return new UserHolder(inflater,viewGroup);
         }
 
         @Override
         public void onBindViewHolder(UserHolder userHolder, int position) {
-            Log.d("SYSTEM INFO: ","МЕТОД onBindViewHolder() UserHolder");
             String userName = userList.get(position);
             userHolder.bind(userName);
-
         }
 
         @Override
         public int getItemCount() {
-            Log.d("SYSTEM INFO: ","МЕТОД getItemCount() UserHolder");
             return userList.size();
         }
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("SYSTEM INFO: ", "Метод onStart() запущен");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("SYSTEM INFO: ", "Метод onSaveInstanceState() запущен");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("SYSTEM INFO: ", "Метод onPause() запущен");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("SYSTEM INFO: ", "Метод onStop() запущен");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("SYSTEM INFO: ", "Метод onDestroy() запущен");
     }
 }
